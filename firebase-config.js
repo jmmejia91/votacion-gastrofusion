93994ae4526147820457c17beeeb64634ef26901
jmmejia91 <juan.mejia.rueda@gmail.com>
2025-09-10 20:46:10 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB81egHqwx7Pv__N13oHdGuuz0dSZys4uw",
+  authDomain: "gastrofusion-votos-f7b4c.firebaseapp.com",
+  databaseURL: "https://gastrofusion-votos-f7b4c-default-rtdb.firebaseio.com",
+  projectId: "gastrofusion-votos-f7b4c",
+  storageBucket: "gastrofusion-votos-f7b4c.firebasestorage.app",
+  messagingSenderId: "827038929252",
+  appId: "1:827038929252:web:2681fdffa14ad8282eea71",
+  measurementId: "G-G62CMEER1X"
+};
